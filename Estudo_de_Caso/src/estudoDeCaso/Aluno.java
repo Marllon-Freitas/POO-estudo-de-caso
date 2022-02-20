64c4fef7a2f08f7dfec529b8748d6a5443be771c
Marllon-Freitas <marllonfreitas64@gmail.com>
2022-02-20 15:23:03 -0300

    Adding a new value to the Student class

--- Estudo_de_Caso/src/estudoDeCaso/Aluno.java
+++ Estudo_de_Caso/src/estudoDeCaso/Aluno.java
@@ -5,14 +5,21 @@ import estudoDeCaso.enums.EscolaridadeEnum;
 public class Aluno extends Pessoa {
 	//Variáveis
 	private EscolaridadeEnum escolaridade;
+	private Curso cursando;
 
 	//Métodos
 	public EscolaridadeEnum getEscolaridade() {
 		return escolaridade;
 	}
-
 	public void setEscolaridade(EscolaridadeEnum escolaridade) {
 		this.escolaridade = escolaridade;
+	}
+	
+	public Curso getCursando() {
+		return cursando;
+	}
+	public void setCursando(Curso cursando) {
+		this.cursando = cursando;
 	}
 	
 }
